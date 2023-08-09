dccd3c4f0960b0b382018543fbf12d6007c65fbb
Leandro Rodrigues santos <learodri@c2r3s5.42lisboa.com>
2023-08-09 21:11:57 +0100

    neww

--- miniheader.h
+++ miniheader.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   miniheader.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: learodri@student.42.fr <learodri>          +#+  +:+       +#+        */
+/*   By: learodri <learodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 17:52:42 by learodri          #+#    #+#             */
-/*   Updated: 2023/08/09 16:28:44 by learodri@st      ###   ########.fr       */
+/*   Updated: 2023/08/09 21:11:26 by learodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ typedef struct s_shell
 	int exit_s;
 	int	nb_pipe;
 	int	expand;
+	struct t_token *head;
 }t_shell;
 
 
--- rest/token.c
+++ rest/token.c
@@ -3,26 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   token.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: learodri@student.42.fr <learodri>          +#+  +:+       +#+        */
+/*   By: learodri <learodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 17:25:12 by learodri@st       #+#    #+#             */
-/*   Updated: 2023/08/09 16:29:16 by learodri@st      ###   ########.fr       */
+/*   Updated: 2023/08/09 21:04:33 by learodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../miniheader.h"
 
-//    echo "vai carai seu viado" | wc -l > aqui.txt 
+// echo "vai carai seu viado" | wc -l > aqui.txt 
+
+//echo "vai carai seu viado"|wc -l >aqui.txt 
 
 //echo "vai carai" > vai.txt
 
 //lanca na linked list
 
-char *create_it(char *in, int *i)
+
+
+void	create_it(char *in, int *i)
 {
+	char	tmp[999];
+	char c;
+	
 	while (in[*i] && (in[*i] =! ' ' || in[*i] != '\t'))
 	{
-		i++;
+		if (in[*i] == 39 || in[*i] == '"')
+		{
+			tmp[*i] = in[*i]; 
+			c = in[*i];
+			i++;
+			while (in[*i] != c) // cai dentro das aspas
+			{
+				tmp[*i] = in[*i];
+				i++;
+			}
+			tmp[*i + 1] = c;
+			tmp [*i + 2] = '\0';
+		}
+		
+		if (in[*i])
+			i++;
 	}
 }
 
@@ -30,10 +52,10 @@ void	token_it(char *in, int i) // mesmo contadorr??
 {
 	int *i;
 
-	while (in[i])
+	while (in[i] && (in[i] == ' ' || in[i] == '\t'))
 	{
-		while (in[i] && (in[i] == ' ' || in[i] == '\t'))
-			i++;
+		i++;
 		create_it(in, &i);
 	}
+	
 }
