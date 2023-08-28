0fcf0ba372dd205ae8af99828d68e2f5afbbda41
Le Santos <leandrotyr@gmail.com>
2023-08-28 20:44:31 +0100

    vai carai

--- rest/start_arvo.c
+++ rest/start_arvo.c
@@ -6,7 +6,7 @@
 /*   By: learodri@student.42.fr <learodri>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 18:50:55 by learodri          #+#    #+#             */
-/*   Updated: 2023/08/28 16:54:30 by learodri@st      ###   ########.fr       */
+/*   Updated: 2023/08/28 20:43:34 by learodri@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,18 +37,60 @@ t_node *newnode()
 
 }
 
-t_token *for_cmd(t_token *start) // contar quantas strs para matriz, 
+t_token *for_cmd(t_token *start) // contar quantas strs para matriz, ja manda pra arvore, fodase
 {
 	t_token *tmp;
+	t_node *new;
+	int	nb_str;
+	int i = 0;
 	
+	tmp = start;
+	new = malloc(sizeof(t_node));
+	if (!new)
+		return ;
+
+	while (tmp)
+	{
+		if (tmp->type == 0)
+			nb_str++;
+		tmp = tmp->next;
+	}
+	new->nodeType = E_CMD; //checar depois
+	new->left = NULL;
+	new->right = NULL;
+	new->up = NULL;
+	new->arguments = malloc(sizeof(char *) * (nb_str + 1));
+	if (!new->arguments) //voltar aqui para o final free talbez
+	{
+		if (new)
+			free(new);
+		return ; 
+	}
+	//new->pipe[]
 	tmp = start;
 
 	while (tmp)
 	{
 		if (tmp->token[0] == '|')
+		{
+			new->arguments[i] = NULL;
+			//sent_to_tree(new); - node tem que tar 100% pronto
 			return(tmp);
-		
+		}
+		if (tmp->type == 0)
+		{
+			new->arguments[i] = ft_strdup(tmp->token);
+			if (!new->arguments[i])
+			display_error("deu ruim na mtx node-arvore", 0);
+			i++;
+		}
+		tmp = tmp->next;
 	}
+
+	new->arguments[i] = NULL;
+	//sent_to_tree(new);
+
+	return NULL; // em caso de nao ter mais pipes ou nao ter
 	
 }
 
@@ -67,7 +109,7 @@ t_node *token_tree(t_token *head)
 
 	if (pipe)
 	{
-		insert_tr(new_node(pipe));
+		sent_to_tree(new_node(pipe));
 		token_tree(pipe);
 	}
 
