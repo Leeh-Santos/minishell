1cfce46bad34d13490c36a569e92337483f8a8ca
Miguel Simoes de oliveira barreto <msimoes-@c2r7s4.42lisboa.com>
2023-08-29 20:09:24 +0100

    input fix

--- rest/inputcheck.c
+++ rest/inputcheck.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   inputcheck.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msimoes- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: msimoes- <msimoes-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 17:56:02 by learodri          #+#    #+#             */
-/*   Updated: 2023/08/28 03:26:43 by msimoes-         ###   ########.fr       */
+/*   Updated: 2023/08/29 20:07:37 by msimoes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,39 +42,32 @@ void	quotecheck(char *input, int i, int flag)
 
 void		verify_c(char c, int i, char *in)
 {
-	int flag1;
-	int flag2;
+	int flag;
 
-	flag1 = 0;
-	flag2 = 0;
-	if(in[i - 2] == '<' || in[i - 2] == '>')
-		flag2++;
+	flag = 0;
 	while((in[i]) && (in[i] == '\t' || in[i] == ' ')){
 		if ((c == '>' || c == '<') && (in[i] == '\t' || in[i] == ' '))
-			flag1++;
+			flag++;
 		i++;
 	}
-	//falta: "echo |< ola" & "echo |> ola" funcionar mas "echo |<" & "echo |>" não pode funcionar, e o mesmo para ">>" & "<<"
+	//falta: echo ola |>> |<<
 	if (c == '|' && (in[i] == '|'))
 		display_error("check pipes or redirects syntax error", 0);
+	if (c == '|' && ((in[i] == '>') || (in[i] == '<')))
+	{
+		while((in[i++]) && (in[i] == '\t' || in[i] == ' '))
+			i++;
+		if(char_checker(in[i]))
+			display_error("check pipes or redirects syntax error", 0);
+	}
 	if (c == '<' && (in[i] == '>' || in[i] == '|'))
 		display_error("check pipes or redirects syntax error", 0);
-	if (c == '>' && (in[i] == '>') && flag1)
-		display_error("espaco entre redir", 0);
-	if (c == '<' && (in[i] == '<') && flag1)
-		display_error("espaco entre redir", 0);
 	if (c == '>' && (in[i] == '<' || in[i] == '|'))
-	{
-		if(in[i++] == '|')
-		{
-			while((in[i]) && (in[i] == '\t' || in[i] == ' '))
-				i++;
-			if(char_checker(in[i]) || flag2)
-				display_error("check pipes or redirects syntax error", 0);
-		}
-		else
-			display_error("check pipes or redirects syntax error", 0);
-	}	
+		display_error("check pipes or redirects syntax error", 0);
+	if (c == '>' && (in[i] == '>') && flag)
+		display_error("espaco entre redir", 0);
+	if (c == '<' && (in[i] == '<') && flag)
+		display_error("espaco entre redir", 0);
 }
 
 void	redicheck(char *input, int i)
