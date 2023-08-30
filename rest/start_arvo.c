8be39c307199f9d11e41810e0ce636a42861f1d5
Le Santos <leandrotyr@gmail.com>
2023-08-30 12:59:10 +0100

    falta testar

--- rest/start_arvo.c
+++ rest/start_arvo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   start_arvo.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: learodri <learodri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: learodri@student.42.fr <learodri>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 18:50:55 by learodri          #+#    #+#             */
-/*   Updated: 2023/08/29 19:47:06 by learodri         ###   ########.fr       */
+/*   Updated: 2023/08/30 12:58:52 by learodri@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,8 @@ void	token_type(void)
 
 	while(tmp)
 	{
+		if (tmp->token[0] == '|')
+			tmp->type = -1;
 		if (tmp->token[0] == '<' || tmp->token[0] == '>')
 		{
 			tmp->type = 1;
@@ -54,8 +56,7 @@ void	token_type(void)
 void	send_to_tree(t_node *node)
 {
 	t_node *tmp = node;
-
-	printf("nodetipo %d - 1str %s - 2str %s \n", node->nodeType, node->arguments[0], node->arguments[1]);
+	printf("treenodetype %d - 1str %s - 2str %s \n", tmp->nodeType, tmp->arguments[0], tmp->arguments[1]);
 	
 }
 
@@ -70,7 +71,7 @@ t_token *for_cmd(t_token *start) // contar quantas strs para matriz, ja manda pr
 	tmp = start;
 	new = malloc(sizeof(t_node));
 	if (!new || !tmp)
-		return ;
+		return NULL;
 	new->nodeType = built_ou_cmd(tmp);
 	new->left = NULL;
 	new->right = NULL;
@@ -83,12 +84,12 @@ t_token *for_cmd(t_token *start) // contar quantas strs para matriz, ja manda pr
 		tmp = tmp->next;
 	}
 
-	new->arguments = malloc(sizeof(char *) * (nb_str + 1));
+	new->arguments = malloc(sizeof(char *) * (nb_str + 1)); ///verify here
 	if (!new->arguments) //voltar aqui para o final free talbez
 	{
 		if (new)
 			free(new);
-		return ; 
+		return NULL; 
 	}
 	new->pipe[0] = -1;
 	new->pipe[1] = -1;
@@ -99,7 +100,7 @@ t_token *for_cmd(t_token *start) // contar quantas strs para matriz, ja manda pr
 		if (tmp->token[0] == '|')
 		{
 			new->arguments[i] = NULL;
-			sent_to_tree(new); 
+			send_to_tree(new); 
 			return(tmp);
 		}
 		if (tmp->type == 0)
@@ -113,7 +114,7 @@ t_token *for_cmd(t_token *start) // contar quantas strs para matriz, ja manda pr
 	}
 
 	new->arguments[i] = NULL;
-	sent_to_tree(new);
+	send_to_tree(new);
 
 	return NULL; // em caso de nao ter mais pipes ou nao ter
 	
@@ -145,16 +146,20 @@ t_node*	redir_node(t_token *token_node, char *arg)
 
 	tmp = token_node;
 	t_node *new = malloc(sizeof(t_node));
-	if (!new)
-		display_error("falha no malloc tree node", 0);
+	
 	new->nodeType = redir_tipo(tmp);
 	new->arguments = malloc(sizeof(char *) * 2);
+	if (!new->arguments || !new)	
+		display_error("falha no malloc tree node", 0);
+	new->arguments[0] = ft_strdup(arg);
+	new->arguments[1] = '\0';
 	new->left = NULL;
 	new->right = NULL;
 	new->up = NULL;
 	new->pipe[0] = -1;
 	new->pipe[1] = -1;
 	
+	return (new);
 }
 
 void	for_redir(t_token *start) // ja manda o tipo do redir, o parsing que q ta ok! se nao da ruim
@@ -170,8 +175,31 @@ void	for_redir(t_token *start) // ja manda o tipo do redir, o parsing que q ta o
 		if (tmp->type == 1)
 		{
 			send_to_tree(redir_node(tmp, tmp->next->token));
+			tmp = tmp->next->next; // > arg
+			continue;
 		}
+		tmp = tmp->next;
 	}
+	
+}
+
+t_node *pipe_node(void)
+{
+
+	t_node *new;
+
+	new = malloc(sizeof(t_node));
+	if (!new)
+		return NULL;
+	new->nodeType = E_PIPE;
+	new->arguments = NULL;	
+	new->left = NULL;
+	new->right = NULL;
+	new->up = NULL;
+	new->pipe[0] = -1;
+	new->pipe[1] = -1;
+
+	return (new);
 	
 }
 
@@ -183,17 +211,17 @@ t_node *token_tree(t_token *head)
 	pipe = NULL;
 	tmp = head;
 	if (!tmp)
-		return;
+		return NULL;
 
-	pipe = for_cdm(tmp); //manda node para arvore
+	pipe = for_cmd(tmp); //manda node para arvore
 	for_redir(tmp); // manda node para arover
 
 	if (pipe)
 	{
-		sent_to_tree(new_node(pipe));
+		send_to_tree(pipe_node());
 		token_tree(pipe);
 	}
-
+	return (shell()->root);
 }
 
 
@@ -208,19 +236,3 @@ t_node *token_tree(t_token *head)
     E_BUILT
 } t_type;*/
 
-/*while (tmp)
-	{
-		flag = 2;
-		while (flag != 0 && (tmp->token[0] != '|' || tmp))
-		{
-			if (tmp->token == '|' || tmp->next == NULL)
-			{
-				flag--;
-				if(tmp->token == '|' && flag == 0)
-				{
-					printf("por node");
-				}
-			}
-		}
-		
-	}*/
