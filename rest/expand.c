4ca0516e3c89c3e39f05f20efad69d12e95c802a
Miguel Simoes de oliveira barreto <msimoes-@c2r2s3.42lisboa.com>
2023-09-12 20:34:47 +0100

    more logic

--- rest/expand.c
+++ rest/expand.c
@@ -23,19 +23,28 @@ static char *do_expand(char *str, char **env, int i)
 char *expand_check(char *in, char **env)
 {
     int i;
+    char c;
     char *ret;
 
     ret = in;
     i = -1;
+    c = 0;
     while (ret[++i])
     {
-        if (ret[i] == '$' && ret[i + i])
+        if((ret[i] == '\'' || ret[i] == '\"'))
+        {
+            if(!c)
+                c = ret[i];
+            else if(c == ret[i])
+                c = 0;
+        }
+        if ((ret[i] == '$' && ret[i + i]) && c == '\"')
         {
             ret = do_expand(ret, env, i);
             i = -1;
         }
     }
+
     return ret;
 }
 
-//função de apagar quotes
