f738a758e7cf3e4f18ff9af94be18e236761624b
ms1moes <barretoguel@gmail.com>
2023-11-22 00:25:12 +0000

    quote bug fixed

--- rest/utils2.c
+++ rest/utils2.c
@@ -12,30 +12,74 @@
 
 #include "../miniheader.h"
 
-char	*del_quotes(char *str)
+int	count_quotes(char *str)
 {
 	int		i;
 	int		j;
-	char	*ret;
+	char	c;
 
 	i = -1;
 	j = 0;
-	while (str[++i])
-		if (str[i] == '\'' || str[i] == '\"')
-			j++;
-	if (j == 0)
-		return (str);
-	ret = malloc(sizeof(char) * ((ft_strlen(str) - j) + 1));
-	if (!ret)
-		return (0);
-	i = -1;
-	j = -1;
+	c = 0;
 	while (str[++i])
 	{
-		if (str[i] != '\'' && str[i] != '\"')
-			ret[++j] = str[i];
+		if ((str[i] == '\'' || str[i] == '\"'))
+		{
+			if (!c)
+				c = str[i];
+			else if (c == str[i])
+			{
+				c = 0;
+				j++;
+			}
+		}
+		if ((str[i] == '\'' && c == '\'') || (str[i] == '\"' && c == '\"'))
+			j++;
 	}
-	ret[++j] = 0;
+	return (j);
+}
+
+char	*quote_cleaner(char *str, char *ret)
+{
+	int		i;
+	int		j;
+	char	c;
+
+	i = -1;
+	j = 0;
+	c = 0;
+	while (str[++i])
+	{
+		if ((str[i] == '\'' || str[i] == '\"'))
+		{
+			if (!c)
+				c = str[i];
+			else if (c == str[i])
+			{
+				c = 0;
+				continue ;
+			}
+		}
+		if ((str[i] == '\'' && c == '\'') || (str[i] == '\"' && c == '\"'))
+			continue ;
+		ret[j++] = str[i];
+	}
+	ret[j] = 0;
+	return (ret);
+}
+
+char	*del_quotes(char *str)
+{
+	int		delete;
+	char	*ret;
+
+	delete = count_quotes(str);
+	if (delete == 0)
+		return (str);
+	ret = malloc(sizeof(char) * ((ft_strlen(str) - delete) + 1));
+	if (!ret)
+		return (0);
+	ret = quote_cleaner(str, ret);
 	free(str);
 	return (ret);
 }
